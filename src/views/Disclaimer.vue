3ab0e6487369f99ed2700d5ef2e48418fc3870a0
Zainab Babayaro <babayarozainab@gmail.com>
2023-12-05 15:09:07 +0100

    disclaimer

--- src/views/Disclaimer.vue
+++ src/views/Disclaimer.vue
@@ -58,23 +58,95 @@
                     availability. FNX Dublin Limited also disclaims liability for errors and omissions in this site and its
                     contents.</p>
             </section>
-
-
-            <section class=" space-y-5 p-7">
-                <h3 class="font-semibold text-[16px] ">Our Approach</h3>
-                <p class=" text-[12px]">Our approach to servicing your needs is characterized by:</p>
-                <ul class=" text-[12px] space-y-4 list-decimal">
-                    <li> <b>Client-Centric Solutions:</b> We tailor our services to meet your unique requirements,
-                        ensuring that
-                        your goals are our top priority.
-                    </li>
-                    <li><b>Strategic Insights:</b> Our team of seasoned professionals provides strategic insights and
-                        recommendations based on market trends and regulatory changes.</li>
-                    <li><b>Transparency</b> We maintain open and clear communication with our clients throughout our
-                        partnership,
-                        offering complete transparency in all our services.</li>
-                </ul>
+            <section class="space-y-5 p-7">
+                <h3 class="font-semibold text-[18px] pt-5">Liability and Indemnity</h3>
+                <p class="text-[12px]  md:text-[18px] "> In no event shall FNX Dublin Limited , its directors or employees
+                    be liable for any claims, liabilities, losses, costs or damages of any kind, including (i) direct,
+                    indirect, consequential, punitive, exemplary, incidental or special losses or (ii) loss of profit, loss
+                    of business, business interruption, loss of business opportunity or loss of data arising out of or in
+                    any way connected with (a) your use of or inability to access and/or use this site, (b) the contents of
+                    this site, or (c) the contents of any third party sites linked or linking to this site. The foregoing
+                    applies even if FNX Dublin Limited, its directors or employees have been advised of the possibility of
+                    such claims, liabilities, losses, costs or damages or could have foreseen them. FNX Dublin Limited, its
+                    directors and employees shall not be responsible for any failure by you or any third party to comply
+                    with these terms or with applicable law. If any part of this term is unenforceable under applicable law,
+                    the aggregate liability of FNX Dublin Limited, its directors and employees will be limited to the
+                    maximum extent permitted by such law and shall in no event exceed EUR100.
+                    Your sole remedy for dissatisfaction with your use of this site is to cease using this site.
+                    You agree, at your own expense, to indemnify, defend and hold FNX Dublin Limited, its directors, members
+                    and employees harmless from and against any claims, liabilities, losses, costs or damages of any kind
+                    (including any legal fees and costs) suffered or incurred by these parties in connection with any claim
+                    arising out of any breach of these terms by you. You shall cooperate as fully as reasonably required or
+                    requested in the defence of any such claim. FNX Dublin Limited may, at its own expense, assume the
+                    exclusive defence and control of any matter otherwise subject to indemnification by you and you shall
+                    not in any event settle any matter without the written consent of FNX Dublin Limited.</p>
             </section>
+            <section class="space-y-5 p-7">
+                <h3 class="font-semibold text-[18px] pt-5">Third Party Sites</h3>
+                <p class="text-[12px]  md:text-[18px] "> FNX Dublin Limited is not responsible for the contents of any third
+                    party sites linked or linking to this site and disclaims all liability arising from your use of or
+                    reliance on such sites, their contents and any products and services referred to on such sites. Your use
+                    of any such sites is at your own risk. FNX Dublin Limited’s inclusion of links to third party sites does
+                    not imply any endorsement of any kind by FNX Dublin Limited of such third parties, their sites or their
+                    products or services.</p>
+            </section>
+            <section class="space-y-5 p-7">
+                <h3 class="font-semibold text-[18px] pt-5">Prohibited Acts</h3>
+                <p class="text-[12px]  md:text-[18px] "> You must not use any data mining, robots or similar data gathering
+                    or extraction programs or methods, whether automated or manual, to access, acquire, copy or monitor any
+                    portion of this site or any of its contents.
+                    You must not misuse this site by knowingly introducing viruses, trojans, worms, logic bombs or other
+                    material that is malicious or technologically harmful. You must not attempt to gain unauthorised access
+                    to this site, the server on which it is stored or any server, computer or database connected to this
+                    site. You must not attack this site via a denial-of-service attack or a distributed denial-of service
+                    attack. By breaching this provision, you would commit a criminal offence. FNX Dublin Limited will report
+                    any such breach to the relevant law enforcement authorities and will co-operate with those authorities
+                    by disclosing your identity to them. In the event of such a breach, your right to use this site will
+                    cease immediately.</p>
+            </section>
+            <section class="space-y-5 p-7">
+                <h3 class="font-semibold text-[18px] pt-5">General</h3>
+                <p class="text-[12px]  md:text-[18px] ">A failure by FNX Dublin Limited to enforce a provision of these
+                    terms or act on a breach or default by you does not constitute a waiver of any of FNX Dublin Limited’s
+                    rights or remedies.
+                    These terms constitute the entire agreement between FNX Dublin Limited and you with respect of your use
+                    of this site and they supersede all prior or contemporaneous communications, agreements and
+                    understandings between FNX Dublin Limited and you with respect to the subject matter hereof. These terms
+                    are governed by and shall be construed in accordance with the laws of England and Wales. You agree that
+                    the Courts of Ireland shall have exclusive jurisdiction over any disputes arising in relation to use and
+                    browsing of this site.
+                    Please send any questions about these terms or the contents of this site to: enquiries@fnx.ie</p>
+            </section>
+            <section class="space-y-5 p-7">
+                <h3 class="font-semibold text-[18px] pt-5">Cookies</h3>
+                <p class="text-[12px]  md:text-[18px] "> We may collect information about your computer, including where
+                    available your IP address, the company name from which you are browsing our website, operating system
+                    and browser type. This is purely data about our types of users, browsing actions and patterns, and does
+                    not identify any individual unless they provide us with their details. For the same reason, we may
+                    obtain information about your general internet usage by using a cookie file which is stored on the hard
+                    drive of your computer. Cookies contain information that is transferred to your computer's hard drive.
+                    You may refuse to accept cookies by activating the setting on your browser which allows you to refuse
+                    the setting of cookies. However, if you select this setting you may be unable to access certain parts of
+                    our site. They help us to improve our site and to deliver a better and more personalised service. They
+                    enable us:
+                <ul>
+                    <li> To estimate our audience size and usage pattern.</li>
+                    <li> To store information about your preferences, and so allow us to customise our site according to
+                        your
+                        individual interests.</li>
+                    <li> To speed up your searches.
+                    </li>
+                    <li> To recognise you when you return to our site.
+                    </li>
+                    <li> Unless you have adjusted your browser setting so that it will refuse cookies, our system will issue
+                        cookies when you log on to our site. Please note that our partners may also use cookies, over which
+                        we
+                        have no control.</li>
+                </ul>
+                </p>
+            </section>
+
+
         </div>
         <Footer />
 
