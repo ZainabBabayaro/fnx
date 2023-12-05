9e28936b11de0c71650dd3edc194eed939b21186
Zainab Babayaro <babayarozainab@gmail.com>
2023-12-05 16:17:00 +0100

    privacy policy

--- src/views/PrivacyPolicy.vue
+++ src/views/PrivacyPolicy.vue
@@ -1,5 +1,219 @@
 <template>
     <div>
 
+        <div class=" px-12 text-[#002D3C] font-light leading-loose">
+            <div class="flex justify-between items-center pt-6">
+                <img src="../assets/black-logo.svg" alt="" class="w-12">
+                <img src="../assets/black-hamburger.svg" alt="" class="cursor-pointer w-10"
+                    @click="openHamburger = !openHamburger">
+            </div>
+            <h1 class="text-center text-2xl font-semibold">Privacy policy</h1>
+            <section class="space-y-5 p-7">
+                <h3 class="font-semibold text-[18px] pt-5">Overview</h3>
+                <p class="text-[12px]  md:text-[18px] "> Article 5 of the GDPR States that Personal Data must be processed
+                    lawfully fairly and in a transparent manner. In line with the GDPR changes we are updating our Privacy
+                    Notice so you can better understand why and how we collect, process and destroy your data. We are
+                    committed to protecting and respecting your privacy. This policy (together with the Terms and Conditions
+                    and any other documents referred to in it) sets out the legal basis on which any Personal Data we
+                    collect from you, or that you provide to us, will be processed by us. Please read the following
+                    carefully to understand our views and practices regarding your Personal Data and how we will treat it.
+                </p>
+            </section>
+
+            <section class="space-y-5 p-7">
+                <h3 class="font-semibold text-[18px] pt-5">What types of Personal Data do we collect?</h3>
+                <p class="text-[12px]  md:text-[18px] "> We may control, process and use your Personal Data, which may
+                    include names, postal addresses, email addresses, telephone numbers or any other Personal Data that you
+                    provide to us. We may also, in appropriate cases and to the extent permitted by law, control, process
+                    and use certain special categories of Personal Data which are more sensitive in nature (e.g. when
+                    undertaking “Know Your Customer” (KYC) or anti-money laundering (AML) checks, we may collect information
+                    about any criminal conviction offence that you or the directors of any company might have committed).
+                </p>
+            </section>
+
+            <section class="space-y-5 p-7">
+                <h3 class="font-semibold text-[18px] pt-5">Identity of the Firm</h3>
+                <p class="text-[12px]  md:text-[18px] "> FNX Dublin Limited is registered in the Republic of Ireland with
+                    Company Number 682114 and regulated by Central Bank of Ireland with registration number C441669, with
+                    registered office at:
+                    Unit 1, Block 1, Northwood Court, Santry, D09 E438, Ireland.</p>
+            </section>
+
+            <section class="space-y-5 p-7">
+                <h3 class="font-semibold text-[18px] pt-5">Lawful basis for Processing</h3>
+                <p class="text-[12px]  md:text-[18px] "> Where we act as Data Controller, we rely on the following legal
+                    basis for Processing your Personal Data:
+                    Consent - if you are a recipient of our online direct marketing;
+                    Legitimate interests – if you are our client or prospect client, business affiliate, employee or
+                    potential employee, or our website visitor;
+                    Performance of contract- if you are our client, supplier, employee, akin to employee or business
+                    affiliate or our website visitor;
+                    Legal obligation – if we process Personal Data according to requirements of domestic legislation.
+                    Where we act as Data Processor, we process Personal Data on behalf of Data Controller and we act on
+                    their written instructions.</p>
+            </section>
+            <section class="space-y-5 p-7">
+                <h3 class="font-semibold text-[18px] pt-5">Data protection officer</h3>
+                <p class="text-[12px]  md:text-[18px] "> The Firm’s Data protection officer (the “DPO”) may be reached on
+                    privacy@fnx.ie</p>
+            </section>
+            <section class="space-y-5 p-7">
+                <h3 class="font-semibold text-[18px] pt-5">Purpose of data collected</h3>
+                <p class="text-[12px]  md:text-[18px] "> The personal information we collect is for the following legitimate
+                    interest:
+                <ul>
+                    <li> Provision of financial products and services;</li>
+                    <li> Administration of your investments;</li>
+                    <li> Promotion of ideas and events relating to services we provide; </li>
+                    <li> Accuracy of client records </li>
+                    <li> To respond to you enquires; </li>
+                    <li> To comply with any present or future law, rule, regulation, guidance, decision or directive
+                        (including those concerning anti-terrorism, fraud, AML and anticorruption) </li>
+                    <li> To carry out, in appropriate cases, KYC checks and other procedures that we undertake prior to you
+                    </li>
+                    <li> becoming a customer of ours;
+                    </li>
+                    <li> Prevention and detection of fraud and other illegal activity or misconduct; and
+                    </li>
+                    <li> For informing you about compliance with legal and regulatory obligations and provide related
+                        guidance.
+                    </li>
+
+                </ul>
+                </p>
+            </section>
+            <section class="space-y-5 p-7">
+                <h3 class="font-semibold text-[18px] pt-5">Who we share our information with</h3>
+                <p class="text-[12px]  md:text-[18px] "> We will not share personal information about you with third parties
+                    without your consent. We are required, by law, to sometimes pass on some of this Personal Data to:
+                    Law enforcement agencies; financial regulators and other relevant regulatory authorities; government
+                    bodies; tax authorities; courts tribunals and complaints/dispute resolution bodies;
+                    Other bodies as required by law or regulation; or
+                    Related financial institutions such as trustees, custodians and sub-custodians; insurers; fraud
+                    protection agencies; and/or similar suppliers or service providers.
+                    To fulfil our contract with you, the Firm may sometimes pass information to:
+                    IT services including client relationship management platforms; and
+                    Intra group to related affiliates also working on providing you with related services.</p>
+            </section>
+            <section class="space-y-5 p-7">
+                <h3 class="font-semibold text-[18px] pt-5">International transfer outside the EEA</h3>
+                <p class="text-[12px]  md:text-[18px] ">The Firm does not transfer your personal information outside of the
+                    European Economic Area (EEA) unless the transfer is necessary for one of the reasons set out in the GDPR
+                    including the performance of a contract between the Firm and the Data Subject, reasons of public
+                    interest, to establish, exercise or defend legal claims or to protect the vital interests of the Data
+                    Subject where the Data Subject is physically or legally incapable of giving consent and, in some limited
+                    cases, for our legitimate interest.
+                    In these cases, we will follow the GDPR Guidelines in protecting the transfer of data to countries
+                    outside the EEA to ensure that the level of data protection afforded to individuals by the GDPR is not
+                    undermined.
+                    The Firm will only transfer Personal Data outside the EEA if one of the following conditions applies:
+                    The European Commission has issued a decision confirming that the country to which we transfer the
+                    Personal Data ensures an adequate level of protection for the Data Subjects’ this refers to
+                    (individual’s resident rights and freedoms);
+                    Appropriate safeguards are in place such as binding corporate rules (BCR), standard contractual clauses
+                    approved by the European Commission, an approved code of conduct or a certification mechanism, a copy of
+                    which can be obtained from the Responsible Officer;
+                    The Data Subject has provided Explicit Consent (Explicit consent is where permission has been given by
+                    the Data Subject in writing to the proposed transfer after being informed of any potential risks.)</p>
+            </section>
+            <section class="space-y-5 p-7">
+                <h3 class="font-semibold text-[18px] pt-5">Retention</h3>
+                <p class="text-[12px]  md:text-[18px] "> We will keep your Personal Data for no longer than reasonably
+                    necessary or required by law. We will retain your personal information in accordance with legal and
+                    regulatory requirements as set out in our Data retention policy.
+
+                </p>
+                <section class="space-y-5 p-7">
+                    <h3 class="font-semibold text-[18px] pt-5">Your rights and your Personal Data</h3>
+
+                    <ul>
+                        <li> You have a right:</li>
+                        <li> To request a copy of your Personal Data which the Firm or related data Controller holds about
+                            you;</li>
+                        <li> To request the Firm or any related data Controller to correct any Personal Data if it is found
+                            to be inaccurate or out of date; </li>
+                        <li> To request your Personal Data is erased where it is no longer necessary for the Firm or related
+                            data Controller to retain such data;</li>
+                        <li> To withdraw your consent to the Processing at any time if consent constitutes the lawful basis
+                            for processing; </li>
+                        <li> To object to Processing based on grounds relating to the Data Subject situation if the
+                            processing is necessary for the performance of a task carried out in the public interest or the
+                            processing is necessary for the purposes of the legitimate interest by us or a third party,
+                            unless such interest is overridden by your fundamental rights and interests; </li>
+                        <li>To request a restriction is placed on further Processing;
+                        </li>
+                        <li> To lodge a complaint with the Data Protection Commission (the Irish Supervisory Authority); you
+                            can contact the Information Commissioners Office at Data Protection Commission, 21 Fitzwilliam
+                            Square South, Dublin 2, D02 RD28, Ireland.
+                        </li>
+                        <li> Not to be subject to a decision based on automated Processing; the Firm does not practice such
+                            decision making.
+                        </li>
+
+
+                    </ul>
+
+                </section>
+                <section class="space-y-5 p-7">
+                    <h3 class="font-semibold text-[18px] pt-5">Further Processing</h3>
+                    <p class="text-[12px]  md:text-[18px] ">Where we may seek to further process your data other than for
+                        the original purpose for which it was collected, the Firm shall only further process such data where
+                        the new Processing is compatible with the original purpose.</p>
+                </section>
+                <section class="space-y-5 p-7">
+                    <h3 class="font-semibold text-[18px] pt-5">Safeguarding measures</h3>
+                    <p class="text-[12px]  md:text-[18px] ">We take your privacy seriously and take every reasonable measure
+                        and precaution to protect and secure your Personal Data. We work hard to protect you and your
+                        information from unauthorised access, alteration, disclosure or destruction and have several layers
+                        of security measures in place, including, without limitation, encryptions.</p>
+                </section>
+                <section class="space-y-5 p-7">
+                    <h3 class="font-semibold text-[18px] pt-5">Safeguarding measures</h3>
+                    <p class="text-[12px]  md:text-[18px] ">We take your privacy seriously and take every reasonable measure
+                        and precaution to protect and secure your Personal Data. We work hard to protect you and your
+                        information from unauthorised access, alteration, disclosure or destruction and have several layers
+                        of security measures in place, including, without limitation, encryptions.</p>
+                </section>
+                <section class="space-y-5 p-7">
+                    <h3 class="font-semibold text-[18px] pt-5">Special Categories of Data (if applicable)</h3>
+                    <p class="text-[12px]  md:text-[18px] ">Owing to the products and services that we offer, such as
+                        performance of KYC and other background checks, we sometimes need to process special categories of
+                        data which are deemed to be more sensitive in nature. Where we collect such information, we will
+                        only request and process the minimum necessary for the specified purpose and identify a compliant
+                        legal basis for doing so. Where we rely on your consent for Processing Special Categories Data, we
+                        will obtain your explicit consent through electronic means.e</p>
+                </section>
+                <section class="space-y-5 p-7">
+                    <h3 class="font-semibold text-[18px] pt-5">Legitimate Interests (if applicable)</h3>
+                    <p class="text-[12px]  md:text-[18px] ">We occasionally process your personal information under the
+                        Legitimate Interests’ legal basis. Where this is the case, we have carried out a Legitimate
+                        Interests’ Assessment (LIA) to ensure that we have weighed your interests and any risk posed to you
+                        against our own and that such interests are proportionate and appropriate such as for the purposes
+                        of HR, marketing and day-to-day operations.</p>
+                </section>
+                <section class="space-y-5 p-7">
+                    <h3 class="font-semibold text-[18px] pt-5">Marketing</h3>
+                    <p class="text-[12px]  md:text-[18px] ">When sending marketing materials to customers, we may have the
+                        option to rely on your consent or legitimate interest.
+                        We only use legitimate interests for marketing if we have assessed that the information being sent
+                        is beneficial to the customer, and have weighed our interests against your own and there is little
+                        to no risk posed, the method and content is non-intrusive, and the material being sent is something
+                        you would usually expect to receive.</p>
+                </section>
+            </section>
+
+
+        </div>
+        <Footer />
+
     </div>
 </template>
+  
+<script setup>
+// @ is an alias to /src
+import Footer from '@/components/Footer.vue'
+import Hero from '@/components/Hero.vue'
+
+
+</script>
+  
--- src/views/SustainabilityDisclosures.vue
+++ src/views/SustainabilityDisclosures.vue
@@ -1,5 +1,159 @@
 <template>
     <div>
 
+        <div class=" px-12 text-[#002D3C] font-light leading-loose">
+            <div class="flex justify-between items-center pt-6">
+                <img src="../assets/black-logo.svg" alt="" class="w-12">
+                <img src="../assets/black-hamburger.svg" alt="" class="cursor-pointer w-10"
+                    @click="openHamburger = !openHamburger">
+            </div>
+            <h1 class="text-center text-2xl font-semibold">Privacy policy</h1>
+            <section class="p-7">
+
+                <p class="text-[12px]  md:text-[18px] "> The European Council and European Parliament Regulation (EU)
+                    2019/2088 (“SFDR” or the “Disclosure Regulation”), seeks to establish a pan-European framework to
+                    facilitate Sustainable Investment. The Disclosure Regulation provides for a harmonised approach in
+                    respect of sustainability-related disclosures to investors within the EEA's financial services sector.
+                    In the absence of such harmonisation, individual EU Member States would be free to adopt divergent
+                    disclosure standards or develop different approaches, resulting in an uneven playing field and/or
+                    creating barriers to entry for asset managers looking to make available financial products within the
+                    internal market of the European Union.
+                    The scope of the Disclosure Regulation is extremely broad, covering a very wide range of financial
+                    products and financial market participants. It seeks to achieve more transparency regarding how
+                    financial market participants integrate Sustainability Risks into their investment decisions and
+                    consideration of adverse sustainability impacts in the investment process. Its objectives are to:
+                <ul>
+                    <li> strengthen protection for investors of financial products;</li>
+                    <li>
+                        improve the disclosures made available to investors from financial market participants; and
+                    </li>
+                    <li>
+                        improve the disclosures made available to investors regarding the financial products, to amongst
+                        other things, enable investors make informed investment decisions.
+                    </li>
+                </ul>
+                For the purposes of the Disclosure Regulation, FNX Dublin Limited meets the criteria of a financial
+                market participant.
+                This document fulfils the disclosure requirements under Articles 3, 4 and 5 of SFDR for SAMI as a
+                financial market participant.
+                In accordance with Article 3 of the SFDR, financial market participants are required to publish
+                information on their strategies for integrating sustainability risks in their investment decision-making
+                processes on their websites.
+                In accordance with Article 4 (1) of the SFDR, financial market participants shall make a statement on
+                due diligence strategies related to the main adverse sustainability impacts of investment decisions on
+                sustainability factors.
+                Furthermore, in accordance with Article 5 (1) of the SFDR, financial market participants shall include
+                in their remuneration policies information on how those policies are consistent with the integration of
+                sustainability risks and shall publish that information on their websites.
+                </p>
+            </section>
+
+            <section class=" p-7">
+                <h3 class="font-semibold text-[18px] pt-5">Sustainable Disclosure for Funds</h3>
+                <p class="text-[12px]  md:text-[18px] ">Under SFDR, funds have to be classified in one of the following
+                    categories:
+                <ul>
+                    <li> Funds with sustainable investment objective (article 9);</li>
+                    <li>
+                        Funds promoting environmental or social characteristics (article 8); and
+                    </li>
+                    <li>
+                        Funds which either only integrate or do not take into account environmentally sustainable economic
+                        activities (article 6).
+                    </li>
+                </ul>
+                <p class="italic font-medium">All funds managed by the Company are Article 6.</p>
+                </p>
+            </section>
+
+            <section class=" p-7">
+                <h3 class="font-semibold text-[18px] pt-5">Principle of do no Significant Harm</h3>
+                <p class="text-[12px]  md:text-[18px] "> The Company takes a “best efforts” approach to assessing the
+                    principle of do no significant harm. The Company will, where appropriate and as disclosed in the
+                    relevant fund documentation, take into account guidance from the Taxonomy Regulation on environmental
+                    objectives.</p>
+            </section>
+
+            <section class=" p-7">
+                <h3 class="font-semibold text-[18px] pt-5">Transparency of Sustainability Risk Policies</h3>
+                <p class="text-[12px]  md:text-[18px] "> This is a publicly available policy.</p>
+            </section>
+
+            <section class=" p-7">
+                <h3 class="font-semibold text-[18px] pt-5">Adverse Sustainability Impact at entity level</h3>
+                <p class="text-[12px]  md:text-[18px] "> In accordance with article 4 of the Regulation (EU) 2019/2088 on
+                    sustainability‐related disclosures in the financial services sector the Company, taking due account of
+                    their size, the nature and scale of their activities and the types of financial products they make
+                    available, considers the principal adverse impacts of their investment decisions on sustainability
+                    factors within the relevant field and/or practices. According to the SDFR, principal adverse impacts
+                    should be understood as those impacts of investment decisions and advice that result in negative effects
+                    on sustainability factors.
+                    The Company takes a “best efforts” approach to assessing the adverse impact of investment decisions on
+                    sustainability. The Company will, where appropriate and as disclosed in the relevant fund documentation,
+                    take into account guidance from the Taxonomy Regulation on environmental objectives.
+
+                </p>
+            </section>
+            <section class="p-7">
+                <h3 class="font-semibold text-[18px] pt-5">Remuneration Policy</h3>
+                <p class="text-[12px]  md:text-[18px] "> The Company acknowledges that sustainability risks may be relevant
+                    to the investments held by the Funds it manages however the Company does not currently take account of
+                    sustainability risks as a separate part of its investment decision making processes.
+                    As such the Company’s Remuneration Policy does not provide for the assessment of variable remuneration
+                    due to performance of sustainable and separately market factors.</p>
+            </section>
+            <section class=" p-7">
+                <h3 class="font-semibold text-[18px] pt-5">Integration of Sustainability Risks</h3>
+                <p class="text-[12px]  md:text-[18px] ">Article 6 Funds are not obliged to account for sustainability risks
+                    and as such the Company does not include these factors in the portfolio management process. However, to
+                    the extent that sustainability risks have an adverse impact on performance of the Funds, they are likely
+                    to be reflected in the overall portfolio management.</p>
+            </section>
+            <section class=" p-7">
+                <h3 class="font-semibold text-[18px] pt-5">Adverse Impact at Financial Product Level</h3>
+                <p class="text-[12px]  md:text-[18px] "> Article 6 Funds are not obliged to account for sustainability risks
+                    and as such the Company does not include these factors in the portfolio management process. However, to
+                    the extent that sustainability risks have an adverse impact on performance of the Funds, they are likely
+                    to be reflected in the overall portfolio management.
+
+                </p>
+
+            </section>
+            <section class=" p-7">
+                <h3 class="font-semibold text-[18px] pt-5">Promotion of ESG factors</h3>
+                <p class="text-[12px]  md:text-[18px] ">The Company manages Article 6 funds, as such no qualification on
+                    environmental or social characteristics is made in pre-contractual disclosures.</p>
+
+            </section>
+            <section class=" p-7">
+                <h3 class="font-semibold text-[18px] pt-5">Promotion of Sustainable Investments</h3>
+                <p class="text-[12px]  md:text-[18px] ">The Company manages Article 6 funds, as such no qualification on
+                    sustainable characteristics is made in pre-contractual disclosures.</p>
+            </section>
+            <section class=" p-7">
+                <h3 class="font-semibold text-[18px] pt-5">Promotion of ESG factors on Website</h3>
+                <p class="text-[12px]  md:text-[18px] ">The Company manages Article 6 funds, as such no qualification on
+                    environmental or social characteristics is made on its website.</p>
+            </section>
+
+            <section class=" p-7">
+                <h3 class="font-semibold text-[18px] pt-5">Promotion of ESG factors in Period Reports</h3>
+                <p class="text-[12px]  md:text-[18px] ">The Company manages Article 6 funds, as such no qualification on
+                    environmental or social characteristics is made in Periodic Reports.</p>
+            </section>
+
+
+        </div>
+        <Footer />
+
     </div>
 </template>
+  
+<script setup>
+// @ is an alias to /src
+import Footer from '@/components/Footer.vue'
+import Hero from '@/components/Hero.vue'
+
+
+</script>
+  
